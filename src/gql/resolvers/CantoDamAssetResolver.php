b15b235e73942d69cad9b4890e7d035826d520bb
Andrew Welch <andrew@nystudio107.com>
2023-07-27 21:44:59 -0400

    refactor: Refactor to use a static map of args & methods

--- src/gql/resolvers/CantoDamAssetResolver.php
+++ src/gql/resolvers/CantoDamAssetResolver.php
@@ -10,9 +10,30 @@ use lsst\cantodamassets\models\CantoFieldData;
 
 class CantoDamAssetResolver extends Resolver
 {
-    /**
-     * @inheritDoc
-     */
+    // List of arguments in the order they should be processed, along with the argument transform method
+    protected static array $argsList = [
+        [
+            'args' => ['where'],
+            'method' => 'spreadArgs',
+        ],
+        [
+            'args' => ['whereNull', 'whereNotNull'],
+            'method' => 'simpleArgs',
+        ],
+        [
+            'args' => ['whereIn', 'whereNotIn', 'whereBetween', 'whereNotBetween'],
+            'method' => 'keyArgs',
+        ],
+        [
+            'args' => ['shuffle', 'sortBy', 'sortByDesc', 'reverse'],
+            'method' => 'simpleArgs',
+        ],
+        [
+            'args' => ['first', 'last', 'except', 'nth', 'random', 'skip'],
+            'method' => 'simpleArgs',
+        ],
+    ];
+
     public static function resolve(mixed $source, array $arguments, mixed $context, ResolveInfo $resolveInfo): mixed
     {
         /** @var ElementInterface $source */
@@ -29,71 +50,35 @@ class CantoDamAssetResolver extends Resolver
 
     protected static function applyArguments(Collection $collection, array $arguments): Collection
     {
-        // Filter by where args
-        $whereArgs = ['where', 'whereNull', 'whereNotNull', 'whereIn', 'whereNotIn', 'whereBetween', 'whereNotBetween'];
-        foreach ($whereArgs as $whereArg) {
-            if (!empty($arguments[$whereArg])) {
-                switch ($whereArg) {
-                    case 'where':
-                        $collection = $collection->where(...$arguments[$whereArg]);
-                        break;
-                    case 'whereNull':
-                        $collection = $collection->whereNull($arguments[$whereArg]);
-                        break;
-                    case 'whereNotNull':
-                        $collection = $collection->whereNotNull($arguments[$whereArg]);
-                        break;
-                    case 'whereIn':
-                        $key = array_shift($arguments[$whereArg]);
-                        $collection = $collection->whereIn($key, $arguments[$whereArg]);
-                        break;
-                    case 'whereNotIn':
-                        $key = array_shift($arguments[$whereArg]);
-                        $collection = $collection->whereNotIn($key, $arguments[$whereArg]);
-                        break;
-                    case 'whereBetween':
-                        $key = array_shift($arguments[$whereArg]);
-                        $collection = $collection->whereBetween($key, $arguments[$whereArg]);
-                        break;
-
-                    case 'whereNotBetween':
-                        $key = array_shift($arguments[$whereArg]);
-                        $collection = $collection->whereNotBetween($key, $arguments[$whereArg]);
-                        break;
+        foreach (static::$argsList as $argList) {
+            foreach ($argList['args'] as $arg) {
+                if (!empty($arguments[$arg])) {
+                    $func = $argList['method'];
+                    $collection = static::$func($collection, $arguments, $arg);
                 }
             }
         }
-
-        // Shuffle or sort or sort-desc or reverse
-        if (!empty($arguments['shuffle'])) {
-            $collection = $collection->shuffle();
-        } else if (!empty($arguments['sortBy'])) {
-            $collection = $collection->sortBy($arguments['sortBy']);
-        } else if (!empty($arguments['sortByDesc'])) {
-            $collection = $collection->sortByDesc($arguments['sortByDesc']);
-        } else if (!empty($arguments['reverse'])) {
-            $collection = $collection->reverse();
-        }
-
-        // First, last, except, nth, random, skip
-        if (!empty($arguments['first'])) {
-            $collection = $collection->shift($arguments['first']);
-        } else if (!empty($arguments['last'])) {
-            $collection = $collection->pop($arguments['last']);
-        } else if (!empty($arguments['except'])) {
-            $collection = $collection->except($arguments['except']);
-        } else if (!empty($arguments['nth'])) {
-            $collection = $collection->nth($arguments['except']);
-        } else if (!empty($arguments['random'])) {
-            $collection = $collection->random($arguments['random']);
-        } else if (!empty($arguments['skip'])) {
-            $collection = $collection->skip($arguments['skip']);
-        }
-
         if (is_array($collection)) {
             $collection = new Collection([$collection]);
         }
 
         return $collection;
     }
+
+    protected static function spreadArgs(Collection $collection, array $arguments, string $arg): Collection
+    {
+        return $collection->$arg(...$arguments[$arg]);
+    }
+
+    protected static function keyArgs(Collection $collection, array $arguments, string $arg): Collection
+    {
+        $key = array_shift($arguments[$arg]);
+        return $collection->$arg($key, $arguments[$arg]);
+    }
+
+    protected static function simpleArgs(Collection $collection, array $arguments, string $arg): Collection
+    {
+        return $collection->$arg($arguments[$arg]);
+    }
+
 }
