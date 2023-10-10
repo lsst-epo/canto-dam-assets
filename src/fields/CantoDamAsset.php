f7454a49ba83e74b0285fe1ece58fb0026889ed1
Andrew Welch <andrew@nystudio107.com>
2023-10-09 21:58:43 -0400

    feat: Implement `::getStaticHtml()` for proper display of revisions

--- src/fields/CantoDamAsset.php
+++ src/fields/CantoDamAsset.php
@@ -75,6 +75,19 @@ class CantoDamAsset extends Field implements PreviewableFieldInterface
             ]);
     }
 
+    public function getStaticHtml(mixed $value, ElementInterface $element = null): string
+    {
+        $view = Craft::$app->getView();
+        $this->registerFieldJavaScript($value, $element);
+        // Render the input template
+        $twigVariables = $this->getFieldRenderVariables($value, $element, false);
+
+        return $view->renderTemplate(
+            '_canto-dam-assets/_components/fieldtypes/CantoDamAsset_input.twig',
+            $twigVariables
+        );
+    }
+
     public function getContentColumnType(): array|string
     {
         return [
@@ -107,50 +120,6 @@ class CantoDamAsset extends Field implements PreviewableFieldInterface
         }
 
         return $value;
-    }
-
-    protected function inputHtml(mixed $value, ElementInterface $element = null): string
-    {
-        /** @var  CantoFieldData $value */
-        $view = Craft::$app->getView();
-        $id = Html::id($this->handle);
-        $namespace = $view->getNamespace();
-        $namespacedId = $view->namespaceInputId($id);
-        // Variables to pass down to our field JavaScript to let it namespace properly
-        $jsonVars = [
-            'id' => $id,
-            'fieldId' => $namespacedId,
-            'name' => $this->handle,
-            'namespace' => $namespacedId,
-            'prefix' => Html::namespaceId('', $namespace),
-            'bodyClass' => self::PICKER_TYPE_CLASS_MAP[$this->cantoAssetPickerType] ?? self::PICKER_TYPE_CLASS_MAP['singleImagePicker'],
-        ];
-        $jsonVars = Json::encode($jsonVars);
-        Craft::$app->getView()->registerJs("$('#{$namespacedId}-field').CantoDamConnector(" . $jsonVars . ");");
-        // In case we want to try to transform this image
-        $previewUrl = $value->cantoAssetData[0]['url']['directUrlOriginal'] ?? null;
-        // The name to subtitle the preview
-        $assetCount = count($value->cantoAssetData);
-        $previewName = $value->cantoId == 0 ? "{$assetCount} images" : $value->cantoAssetData[0]['displayName'] ?? null;
-        $albumName = $value->cantoAlbumData['name'] ?? '';
-        // Render the input template
-        return $view->renderTemplate(
-            '_canto-dam-assets/_components/fieldtypes/CantoDamAsset_input.twig',
-            [
-                'name' => $this->handle,
-                'value' => $value,
-                'fieldId' => $namespacedId,
-                'elementId' => $element->id ?? null,
-                'id' => $id,
-                'element' => Json::encode($element),
-                'namespacedId' => $view->namespaceInputId($id),
-                'previewUrl' => $previewUrl,
-                'previewName' => $previewName,
-                'albumName' => $albumName,
-                'assetCount' => $assetCount,
-                'accessToken' => CantoDamAssets::$plugin->assets->getAuthToken()
-            ]
-        );
     }
 
     public function getTableAttributeHtml(mixed $value, ElementInterface $element): string
@@ -188,13 +157,6 @@ class CantoDamAsset extends Field implements PreviewableFieldInterface
         return [];
     }
 
-    protected function searchKeywords(mixed $value, ElementInterface $element): string
-    {
-        /* @var CantoFieldData $value */
-        $keywords = $value->cantoAssetData->flatten()->values()->filter()->all();
-        return implode(' ', $keywords);
-    }
-
     public function getElementConditionRuleType(): array|string|null
     {
         return null;
@@ -211,6 +173,94 @@ class CantoDamAsset extends Field implements PreviewableFieldInterface
         parent::modifyElementsQuery($query, $value);
     }
 
+    // Protected Methods
+    // =========================================================================
+
+    protected function searchKeywords(mixed $value, ElementInterface $element): string
+    {
+        /* @var CantoFieldData $value */
+        $keywords = $value->cantoAssetData->flatten()->values()->filter()->all();
+        return implode(' ', $keywords);
+    }
+
+    protected function inputHtml(mixed $value, ElementInterface $element = null): string
+    {
+        $view = Craft::$app->getView();
+        $this->registerFieldJavaScript($value, $element);
+        // Render the input template
+        $twigVariables = $this->getFieldRenderVariables($value, $element, true);
+
+        return $view->renderTemplate(
+            '_canto-dam-assets/_components/fieldtypes/CantoDamAsset_input.twig',
+            $twigVariables
+        );
+    }
+
+    /**
+     * Return the JavaScript JSON-encoded variables to pass down to our JavaScript jQuery plugin
+     *
+     * @param $value
+     * @param ElementInterface $element
+     */
+    protected function registerFieldJavaScript($value, ElementInterface $element): void
+    {
+        /** @var  CantoFieldData $value */
+        $view = Craft::$app->getView();
+        $id = Html::id($this->handle);
+        $namespace = $view->getNamespace();
+        $namespacedId = $view->namespaceInputId($id);
+        $jsonVars = Json::encode([
+            'id' => $id,
+            'fieldId' => $namespacedId,
+            'name' => $this->handle,
+            'namespace' => $namespacedId,
+            'prefix' => Html::namespaceId('', $namespace),
+            'bodyClass' => self::PICKER_TYPE_CLASS_MAP[$this->cantoAssetPickerType] ?? self::PICKER_TYPE_CLASS_MAP['singleImagePicker'],
+        ]);
+        Craft::$app->getView()->registerJs("$('#{$namespacedId}-field').CantoDamConnector(" . $jsonVars . ");");
+    }
+
+    /**
+     * Return the Twig variables for rendering the field
+     *
+     * @param $value
+     * @param ElementInterface $element
+     * @param bool $enabled Whether the field is enabled or not
+     * @return array[]
+     */
+    protected function getFieldRenderVariables($value, ElementInterface $element, bool $enabled): array
+    {
+        /** @var  CantoFieldData $value */
+        $view = Craft::$app->getView();
+        $id = Html::id($this->handle);
+        $namespacedId = $view->namespaceInputId($id);
+        // In case we want to try to transform this image
+        $previewUrl = $value->cantoAssetData[0]['url']['directUrlOriginal'] ?? null;
+        // The name to subtitle the preview
+        $assetCount = count($value->cantoAssetData);
+        $previewName = $value->cantoId == 0 ? "{$assetCount} images" : $value->cantoAssetData[0]['displayName'] ?? null;
+        $albumName = $value->cantoAlbumData['name'] ?? '';
+
+        return [
+            'name' => $this->handle,
+            'value' => $value,
+            'fieldId' => $namespacedId,
+            'elementId' => $element->id ?? null,
+            'id' => $id,
+            'element' => Json::encode($element),
+            'namespacedId' => $view->namespaceInputId($id),
+            'previewUrl' => $previewUrl,
+            'previewName' => $previewName,
+            'albumName' => $albumName,
+            'assetCount' => $assetCount,
+            'accessToken' => CantoDamAssets::$plugin->assets->getAuthToken(),
+            'enabled' => $enabled
+        ];
+    }
+
+    /**
+     * @return array
+     */
     protected function getGqlArguments(): array
     {
         return Craft::$app->getGql()->prepareFieldDefinitions([
