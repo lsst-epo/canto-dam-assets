e71078b3657b1edc0c277952707498b3335c111d
Andrew Welch <andrew@nystudio107.com>
2023-11-03 20:44:35 -0400

    feat: Add core `updateContentByCantoId()` method

--- src/services/Assets.php
+++ src/services/Assets.php
@@ -3,15 +3,30 @@
 namespace lsst\cantodamassets\services;
 
 use Craft;
+use craft\base\FieldInterface;
+use craft\db\Table;
+use craft\fields\Matrix;
+use craft\helpers\Db;
+use craft\helpers\ElementHelper;
 use craft\helpers\Json;
 use lsst\cantodamassets\CantoDamAssets;
+use lsst\cantodamassets\fields\CantoDamAsset;
+use lsst\cantodamassets\models\CantoFieldData;
 use yii\base\Component;
+use yii\db\Exception;
 
 /**
  * Assets service
  */
 class Assets extends Component
 {
+    public const CONTENT_COLUMN_KEY_MAPPINGS = [
+        'cantoId' => null,
+        'cantoAlbumId' => 'cantoAlbumId',
+        'cantoAssetData' => 'cantoAssetData',
+        'cantoAlbumData' => 'cantoAlbumData',
+    ];
+
     /**
      *  Private function for using the app ID and secret key to get an auth token
      */
@@ -43,5 +58,70 @@ class Assets extends Component
         Craft::error("An exception occurred in getAuthToken()", __METHOD__);
 
         return Craft::t("_canto-dam-assets", "An error occurred fetching auth token!");
+    }
+
+    public function deleteByCantoId(string $cantoId)
+    {
+        $this->deleteEntryContentByCantoId($cantoId);
+        $this->deleteMatrixContentByCantoId($cantoId);
+        $this->deleteSuperTableContentByCantoId($cantoId);
+        $this->deleteNeoContentByCantoId($cantoId);
+    }
+    
+    protected function deleteEntryContentByCantoId(string $cantoId)
+    {
+        $fields = Craft::$app->getFields()->getFieldsByType(CantoDamAsset::class);
+        $cantoFieldData = Craft::createObject([
+            'class' => CantoFieldData::class,
+            'cantoId' => null,
+            'cantoAlbumId' => null,
+            'cantoAssetData' => [],
+            'cantoAlbumData' => [],
+        ]);
+        $this->updateContentByCantoId($cantoId, $cantoFieldData, $fields, Table::CONTENT);
+    }
+
+    protected function deleteMatrixContentByCantoId(string $cantoId)
+    {
+        $fields = Craft::$app->getFields()->getFieldsByType(Matrix::class);
+        foreach ($fields as $field) {
+            $matrixContentTableName = Craft::$app->getMatrix()->defineContentTableName($field);
+        }
+    }
+
+    protected function deleteSuperTableContentByCantoId(string $cantoId)
+    {
+    }
+
+    protected function deleteNeoContentByCantoId(string $cantoId)
+    {
+    }
+
+    /**
+     * Update the $cantoId entry in the $table for the $cantoDamAssetFields with $cantoFieldData
+     *
+     * @param string $cantoId
+     * @param CantoFieldData $cantoFieldData
+     * @param FieldInterface[] $cantoDamAssetFields
+     * @param string $table
+     * @return void
+     */
+    protected function updateContentByCantoId(string $cantoId, CantoFieldData $cantoFieldData, array $cantoDamAssetFields, string $table): void
+    {
+        $columnKey = self::CONTENT_COLUMN_KEY_MAPPINGS['cantoId'] ?? null;
+        foreach ($cantoDamAssetFields as $cantoDamAssetField) {
+            $queryColumn = ElementHelper::fieldColumnFromField($cantoDamAssetField, $columnKey);
+            $columns = [];
+            foreach (self::CONTENT_COLUMN_KEY_MAPPINGS as $propertyName => $selectColumnKey) {
+                $columns[ElementHelper::fieldColumnFromField($cantoDamAssetField, $selectColumnKey)] = $cantoFieldData->$propertyName;
+            }
+            if ($queryColumn) {
+                try {
+                    $rows = Db::update($table, $columns, [$queryColumn => $cantoId]);
+                } catch (Exception $e) {
+                    Craft::error($e->getMessage(), __METHOD__);
+                }
+            }
+        }
     }
 }
