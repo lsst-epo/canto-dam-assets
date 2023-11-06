c7cb7e0af8f6fdd7ef73539f27586cb553f9730b
Andrew Welch <andrew@nystudio107.com>
2023-11-06 18:06:32 -0500

    fix: Make sure we camelize the keys if an array is being returned, since we normalize them to be camelized         // as GraphQL doesn't support spaces or other special characters in the query params

--- src/gql/types/CantoDamAssetType.php
+++ src/gql/types/CantoDamAssetType.php
@@ -5,6 +5,8 @@ namespace lsst\cantodamassets\gql\types;
 use craft\gql\base\ObjectType;
 use GraphQL\Type\Definition\ResolveInfo;
 use lsst\cantodamassets\gql\interfaces\CantoDamAssetInterface;
+use lsst\cantodamassets\lib\laravel\Collection;
+use yii\helpers\Inflector;
 
 class CantoDamAssetType extends ObjectType
 {
@@ -20,6 +22,13 @@ class CantoDamAssetType extends ObjectType
     protected function resolve(mixed $source, array $arguments, mixed $context, ResolveInfo $resolveInfo): mixed
     {
         $fieldName = $resolveInfo->fieldName;
-        return $source[$fieldName] ?? null;
+        $resolvedData = $source[$fieldName];
+        // Make sure we camelize the keys if an array is being returned, since we normalize them to be camelized
+        // as GraphQL doesn't support spaces or other special characters in the query params
+        if (is_array($resolvedData)) {
+            $collection = new Collection($resolvedData);
+            $resolvedData = $collection->mapWithKeys(fn($value, $key) => [Inflector::camelize($key) => $value])->all();
+        }
+        return $resolvedData ?? null;
     }
 }
