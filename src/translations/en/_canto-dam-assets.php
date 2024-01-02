acd21ee48f6eda319099c1fd0d1ecf1a003acc5d
Andrew Welch <andrew@nystudio107.com>
2024-01-01 22:21:35 -0500

    chore: Added missing translations

--- src/translations/en/_canto-dam-assets.php
+++ src/translations/en/_canto-dam-assets.php
@@ -4,4 +4,15 @@ return [
     "Single Image" => "Single Image",
     "Multiple Images" => "Multiple Images",
     "Whole Album" => "Whole Album",
+    'Canto DAM Album updated' => 'Canto DAM Album updated',
+    'What Canto assets can be chosen by this field.' => 'What Canto assets can be chosen by this field.',
+    'Canto DAM Album update failed' => 'Canto DAM Album update failed',
+    'Canto Asset Picker Type' => 'Canto Asset Picker Type',
+    'An error occurred fetching auth token!' => 'An error occurred fetching auth token!',
+    'Canto DAM Asset deleted' => 'Canto DAM Asset deleted',
+    'Canto DAM Album deleted' => 'Canto DAM Album deleted',
+    'Canto DAM Asset update failed' => 'Canto DAM Asset update failed',
+    'Canto Dam Asset' => 'Canto Dam Asset',
+    'Path Only' => 'Path Only',
+    'Canto DAM Asset updated' => 'Canto DAM Asset updated'
 ];
