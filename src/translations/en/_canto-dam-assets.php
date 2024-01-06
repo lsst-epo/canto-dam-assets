79c1688164c43c6eb9fc1010cfdc5afe0fcca49d
Andrew Welch <andrew@nystudio107.com>
2024-01-06 00:31:58 -0500

    refactor: Add translations

--- src/translations/en/_canto-dam-assets.php
+++ src/translations/en/_canto-dam-assets.php
@@ -14,5 +14,12 @@ return [
     'Canto DAM Asset update failed' => 'Canto DAM Asset update failed',
     'Canto Dam Asset' => 'Canto Dam Asset',
     'Path Only' => 'Path Only',
-    'Canto DAM Asset updated' => 'Canto DAM Asset updated'
+    'Canto DAM Asset updated' => 'Canto DAM Asset updated',
+    'The hostname for the Canto Tenant, e.g. `rubin.canto.com`' => 'The hostname for the Canto Tenant, e.g. `rubin.canto.com`',
+    'Tenant Host Name' => 'Tenant Host Name',
+    'Authentication Endpoint' => 'Authentication Endpoint',
+    'App ID' => 'App ID',
+    'If the app ID or secret key is needed in the URL include it as <strong>{appId}</strong> or <strong>{secretKey}</strong> with no spaces.</p>' => 'If the app ID or secret key is needed in the URL include it as <strong>{appId}</strong> or <strong>{secretKey}</strong> with no spaces.</p>',
+    'Secret Key' => 'Secret Key',
+    'Webhook Secure Token' => 'Webhook Secure Token'
 ];
