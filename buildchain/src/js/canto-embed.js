a79353a0afe9302378417fffa512e6fbb17f09bb
Kevin Wong <kevin.wong@noirlab.edu>
2026-02-11 09:14:33 -0700

    feat: removes restricted filter, bumps version, and constrains craftcms version to be 4 (not 5)

--- buildchain/src/js/canto-embed.js
+++ buildchain/src/js/canto-embed.js
@@ -16,7 +16,7 @@ let isLoadingComplete = false;
 let _formatDistrict = '';
 const MAX_CONTENT_REQUEST_ITEMS = 100;
 const MAX_ALBUM_REQUEST_ITEMS = 1000;
-const FILTER_BY_APPROVED = "&approval=approved%7Crestricted";
+const FILTER_BY_APPROVED = "&approval=approved";
 let selectedAlbum = null;
 
 /* -----------------canto API start-------------------------------------------------------------*/
